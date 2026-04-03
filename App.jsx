a554d5fd5b39de28e9b6f84b492f2b1644a3cb8c
Vũ Hiền <hien037305@gmail.com>
2026-04-03 13:17:12 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App() {
+return ( <div className="app"> <h1>Paradise Nursery</h1> <p>Welcome to our online plant shop</p> <button>Get Started</button> </div>
+);
+}
+
+export default App;
