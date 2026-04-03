e76d1223fdb26cd7521cabf8d0ad87ca1e79b4b6
Vũ Hiền <hien037305@gmail.com>
2026-04-03 13:12:20 +0700

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AboutUs() {
+return ( <div> <h1>About Paradise Nursery</h1> <p>
+Paradise Nursery is a plant shop offering a wide variety of indoor plants.
+We aim to provide high-quality plants and a seamless shopping experience. </p> </div>
+);
+}
+
+export default AboutUs;
